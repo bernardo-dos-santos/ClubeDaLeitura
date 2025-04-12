08679513510556669266c6d786d641cdc778b16c
Bernardo dos Santos Ferreira <bernardosantospg@gmail.com>
2025-04-12 10:08:48 -0300

    Add project files.

---
+++ ClubeDaLeituraConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ClubeDaLeituraConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
